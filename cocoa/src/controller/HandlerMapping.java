240633447c427f5bf5469568251891d262d71c42
kosta <kosta@kosta-HP>
2016-04-27 16:39:44 +0900

    ㅣㅣㅣ

--- cocoa/src/controller/HandlerMapping.java
+++ cocoa/src/controller/HandlerMapping.java
@@ -18,26 +18,26 @@ public class HandlerMapping {
 		}else if(command.equals("")){
 			c = new ShowContentController();         //공지사항 세부사항 컨트롤러
 		}else if(command.equals("")){
-			c = new BoardWriteController();          //
+			c = new BoardWriteController();          //관리자 공지사항 글쓰기 
 		}else if(command.equals("")){
-			c = new BoardUpdateController();
+			c = new BoardUpdateController();        //공지사항 수정
 			
 		//*******멤버 컨트롤러**********
-		}else if(command.equals("")){
+		}else if(command.equals("")){                   //로그인
 			c = new LoginController();
-		}else if(command.equals("")){
+		}else if(command.equals("")){                   //로그아웃
 			c = new LogoutController();
-		}else if(command.equals("")){
+		}else if(command.equals("")){                   //회원가입
 			c = new RegisterController();
-		}else if(command.equals("")){
+		}else if(command.equals("")){					  //회원수정
 			c = new MemberUpdateController();
-		}else if(command.equals("")){
+		}else if(command.equals("")){                   //회원탈퇴
 			c = new DropMemberController();
 			
 		//**********찜 컨트롤러***********
-		}else if(command.equals("")){
+		}else if(command.equals("")){                   //찜보내기
 			c = new PokeInsertController();
-		}else if(command.equals("")){
+		}else if(command.equals("")){					   //찜확인
 			c = new PokeUpdateController();
 		}
 		return c;
